a701699869cca0e7808dfb68502050a926df1d2e
Novie <Novieh14@yahoo.com>
2026-03-10 14:44:13 +0400

    first push

---
+++ src/main/java/com/codewithneil/store/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.codewithneil.store.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String index() {
+        return "Hello World11";
+    }
+
+}
